99e95a1700652de5aaac55645f6189bfe8d532ca
Dan Meier <me.daniel.meier@gmail.com>
2026-06-08 15:09:07 +0200

    Add floating scroll-to-top button

--- public/js/script.js
+++ public/js/script.js
@@ -2,6 +2,17 @@
   var toggle = document.querySelector('.sidebar-toggle');
   var sidebar = document.querySelector('#sidebar');
   var checkbox = document.querySelector('#sidebar-checkbox');
+  var scrollTopButton = document.querySelector('#scroll-top');
+
+  function toggleScrollTopButton() {
+    if (!scrollTopButton) return;
+
+    if (window.scrollY > 280) {
+      scrollTopButton.classList.add('is-visible');
+    } else {
+      scrollTopButton.classList.remove('is-visible');
+    }
+  }
 
   document.addEventListener('click', function(e) {
     var target = e.target;
@@ -12,4 +23,13 @@
 
     checkbox.checked = false;
   }, false);
+
+  if (scrollTopButton) {
+    scrollTopButton.addEventListener('click', function() {
+      window.scrollTo({ top: 0, behavior: 'smooth' });
+    });
+
+    window.addEventListener('scroll', toggleScrollTopButton, { passive: true });
+    toggleScrollTopButton();
+  }
 })(document);
